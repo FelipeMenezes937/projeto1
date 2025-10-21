af11518b712b09a6b01cfb66a5841a7e45c4e68d
felipemenezes937 <145990627+FelipeMenezes937@users.noreply.github.com>
2025-10-20 22:20:29 -0300

    testeJS

---
+++ js/script.js
@@ -0,0 +1,24 @@
+function troca(cor) {
+  document.body.style.background = cor;
+}
+
+let imagens = [
+  './assets/img/1p.jpg',
+  './assets/img/2p.jpg',
+  './assets/img/3p.jpg',
+  './assets/img/4p.jpg'
+];
+let tempo = 4000;
+
+function slideShow() {
+  let i = 0;
+  setInterval(function () {
+    if (i >= imagens.length) {
+      i = 0; // reinicia o contador
+    }
+    document.getElementById('img').innerHTML = '<img src="' + imagens[i] + '">';
+    console.log(imagens[i]);
+    i++;
+  }, tempo);
+}
+slideShow();
